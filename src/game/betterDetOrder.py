af7413608b78938b0fe75a3bd898d7f46ca20f3f
Kevin Zhang <kevinleezhang@gmail.com>
2015-10-17 15:29:59 -0400

    Created a better detOrder

--- src/game/betterDetOrder.py
+++ src/game/betterDetOrder.py
@@ -12,11 +12,11 @@ def detOrder(self, state, Stations):
 
 	listOrders = []
 
-	for st in Stations:
-		pending_orders = state.get_pending_orders()
-		while (len(pending_orders) != 0):
-			incomeDict = {}
-	        for order in pending_orders:
+	remaining_orders = state.get_pending_orders()
+	while(len(remaining_orders) != 0)
+		incomeDict = {}
+		for st in Stations:
+	        for order in remaining_orders:
 	        	node = order.get_node()
 	        	money = get_money(order)
 	        	time_created = get_time_created(order)
@@ -24,19 +24,19 @@ def detOrder(self, state, Stations):
 		         	cur_length = nx.shortest_path_length(G, st, node)
 		            max_Income = money - (self.get_time() - time_created + cur_length) * \
 		            DECAY_FACTOR
-		            incomeDict[max_Income] = order;
+		            incomeDict[max_Income] = (st,order);
 		     	except NetworkXError:
 		     		# no possible paths
-		    if(incomeDict.len() == 0):
-		    	break
-			incomeList = incomeDict.keys()
-			sort(incomeList)
-			bestOrder = incomeDict[incomeList.pop()]
-			path = nx.shortest_path(G, st, bestOrder.node())
-			listOrders.append((bestOrder,path))
-			for (u, v) in self.path_to_edges(path):
-                G.remove_edge(u,v)
-                G.remove_edge(v,u)
-            pending_orders.remove(bestOrder)
+	    if(incomeDict.len() == 0):
+	    	break
+		incomeList = incomeDict.keys()
+		sort(incomeList)
+		(st,order) = incomeDict[incomeList.pop()]
+		path = nx.shortest_path(G, st, order.node())
+		listOrders.append((st,order,path))
+		for (u, v) in self.path_to_edges(path):
+            G.remove_edge(u,v)
+            G.remove_edge(v,u)
+        remaining_orders.remove(order)
 
     return listOrders
